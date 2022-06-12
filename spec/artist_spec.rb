1d262c771301a946f476ad8d8aa4315418e896c5
Joel Grant <joelmgrant@me.com>
2022-06-11 18:19:44 -0600

    Test: create testing for basic artist attributes

---
+++ spec/artist_spec.rb
@@ -0,0 +1,37 @@
+require './lib/photograph'
+require './lib/artist'
+
+RSpec.describe Artist do
+  before(:each) do
+    attributes = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    @artist = Artist.new(attributes)
+  end
+
+  describe '#initialize' do
+    it 'returns the artists id' do
+      expect(@artist.id).to eq("2")
+    end
+
+    it 'returns the artists name' do
+      expect(@artist.name).to eq("Ansel Adams")
+    end
+
+    it 'returns the artists birth year' do
+      expect(@artist.born).to eq("1902")
+    end
+
+    it 'returns the artists year of death' do
+      expect(@artist.died).to eq("1984")
+    end
+
+    it 'returns the artists country of origin' do
+      expect(@artist.country).to eq("United States")
+    end
+  end
+end
